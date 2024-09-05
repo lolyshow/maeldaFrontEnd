ae5128bdd273fa711d30fb75fbc7501c37278ab5
Phillip Olalere <phillipolalere@Phillips-MBP.home>
2024-09-05 23:33:40 +0100

    designed product listing, Hero and Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./components/hero/hero.component";
+import { ButtonComponent } from "./components/button/button.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, ButtonComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor() {}
+
   title = 'maeldaFrontEnd';
+  dummyProduct = [
+    {
+      id:1,
+      name:"Meat",
+      image:"",
+      rating:4,
+    },
+    {
+      id:2,
+      name:"Meat",
+      image:"",
+      rating:4,
+    },
+    {
+      id:3,
+      name:"Meat",
+      image:"",
+      rating:4,
+    },
+    {
+      id:4,
+      name:"Meat",
+      image:"",
+      rating:4,
+    },
+
+]
 }
